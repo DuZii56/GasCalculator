fa3f936810e024919c6696001949a9a3f1edc242
Rob Dusing <55949742+DuZii56@users.noreply.github.com>
2019-10-10 15:24:12 -0400

    Add files via upload

---
+++ GasCalculator.py
@@ -0,0 +1,38 @@
+#Author: DuZii56
+#This simple program will add up the total amount of entries a user inputs for their trips and gives a budget to spend on gas.
+#https://github.com/DuZii56/GasCalculator.git
+
+from os import system, name
+
+def clear():
+    _ = system('clear')
+clear()
+
+Allowence = []
+
+def main():
+    entries = 0
+    quit = False
+    
+    answer = input('Press <enter> to continue')
+   
+    clear()
+    while (not quit):
+        print ('Number of entries: ', (entries))
+        MD = float(input('\nEnter miles driven: '))
+        MPG = float(input('Enter MPG: '))
+        PoG = float(input('Enter the current price of gas: '))          
+
+        budget = round(((MD / MPG) * PoG), 2)
+        Allowence.append (budget)
+        
+        entries = entries + 1
+        answer = input('\nContinue? Y/n ')
+        if answer == 'n':
+            Total = sum(Allowence)
+            print ('\nTotal entires: ', (entries))
+            print ('Your total is: $', (Total))
+            quit = True
+    
+main()
+input('\nPress <enter> to exit')
