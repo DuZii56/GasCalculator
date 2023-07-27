89a21961182c68dd783c1f2e3cf3c306b5c5f971
Rob Dusing <55949742+DuZii56@users.noreply.github.com>
2023-07-27 14:25:15 -0400

    Update GasCalculator.py
    Simplified and cleaned up.

--- GasCalculator.py
+++ GasCalculator.py
@@ -1,40 +1,11 @@
 #Author: DuZii56
-#This simple program will add up the total amount of entries a user inputs for their trips and gives a budget to spend on gas.
+#This simple program will give the user a budget to spend on gas with the information given by the user.
 #https://github.com/DuZii56/GasCalculator.git
 
-from os import system, name
+def main():   
+    miles_driven = float(input('\nEnter miles driven: '))
+    miles_per_gallon = float(input('Enter MPG: '))
+    price_of_gas = float(input('Enter the current price of gas: '))          
+    print(f'Your total is: ${round(((miles_driven / miles_per_gallon) * price_of_gas), 2)}')
 
-#This will only work for a Linux machine.  Find proper code to run on a Windows machine.
-def clear():
-    _ = system('clear')
-clear()
-
-Allowence = []
-
-def main():
-    entries = 0
-    quit = False
-    
-    answer = input('Press <enter> to continue')
-   
-    clear()
-    while (not quit):
-        print ('Number of entries: ', (entries))
-        #Find a better way to clean this up.
-        MD = float(input('\nEnter miles driven: '))
-        MPG = float(input('Enter MPG: '))
-        PoG = float(input('Enter the current price of gas: '))          
-
-        budget = round(((MD / MPG) * PoG), 2)
-        Allowence.append (budget)
-        
-        entries = entries + 1
-        answer = input('\nContinue? Y/n ')
-        if answer == 'n':
-            Total = sum(Allowence)
-            print ('\nTotal entires: ', (entries))
-            print ('Your total is: $', (Total))
-            quit = True
-    
 main()
-input('\nPress <enter> to exit')
